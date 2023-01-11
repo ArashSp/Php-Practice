55471b7f6e777a95bb4d8a3c640a29b7ac1f194c
ArashSp <a.sepahy25@gmail.com>
2023-01-11 16:43:10 +0330

    Added The Files

---
+++ feedback/about.php
@@ -0,0 +1,8 @@
+<?php include 'inc/header.php'; ?>
+  
+    <h2>About</h2>
+    <br />
+    <p class="text-center">This Project is the fruit of me watching 3-4 5h crash courses ......    </p>
+    <p class="text-center">it was very painful .....   i hope i never have to write php ever again but it was a nice experience  </p>
+    <p class="text-center">ALSO THIS PROJECT IS PROTECTED BY اراده و تقوا و ایمان شما PLEASE DONT COPY OR SELL  </p>
+<?php include 'inc/footer.php'; ?>
